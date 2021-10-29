95ee1c908695d929e4a5413aff86dde940e2def9
Jason Wang <12841205+bestwbr@users.noreply.github.com>
2021-10-29 05:58:37 -0400

    Add developping tools

---
+++ tools/docker_tools.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+DOCKER_PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+# RUN developping tools from docker
+docker_cmd="docker run -t --rm -e PATH=$DOCKER_PATH:/home/rustenv/.cargo/bin -v $PWD:/home/rustenv/workdir -w /home/rustenv/workdir rustos"
+
+$docker_cmd $@
