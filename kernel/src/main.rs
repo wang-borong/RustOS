caa6e76e3cf69b6350e630998ba130bdbc6bb093
Jason Wang <wangborong@cdjrlc.com>
2021-11-02 00:26:29 +0800

    Prepare to write kernel

---
+++ kernel/src/main.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// 这个函数将在panic时被调用
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
